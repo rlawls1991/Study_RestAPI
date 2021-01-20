eb72bf7d1c21f9002294abc81fdab2a99e48e3a3
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-20 19:04:14 +0900

    test - create event 테스트

---
+++ src/test/java/study/events/EventControllerTests.java
@@ -0,0 +1,57 @@
+package study.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import study.evnets.Event;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .id(10)
+                .name("Spring")
+                .description("Rest API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 01,20, 18, 47 ))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 01,21, 18, 47 ))
+                .beginEventDateTime(LocalDateTime.of(2021, 01,22, 18, 47 ))
+                .endEventDateTime(LocalDateTime.of(2021, 01,23, 18, 47 ))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(200)
+                .location("경기도 안양시 범계")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect( jsonPath("id").exists())
+        ;
+    }
+
+}
