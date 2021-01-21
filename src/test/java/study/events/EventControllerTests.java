f0582431e35dcc94068769aeabb672ad7ebb2060
rlawls1991 <rlawls1991@gmail.com>
2021-01-21 17:23:28 +0900

    test - MockBean을 사용한 테스트

--- src/test/java/study/events/EventControllerTests.java
+++ src/test/java/study/events/EventControllerTests.java
@@ -1,16 +1,20 @@
 package study.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import study.evnets.Event;
+import study.evnets.EventRepository;
 
 import java.time.LocalDateTime;
 
@@ -28,10 +32,12 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
         Event event = Event.builder()
-                .id(10)
                 .name("Spring")
                 .description("Rest API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2021, 01, 20, 18, 47))
@@ -45,6 +51,8 @@ public class EventControllerTests {
                 .free(true)
                 .offline(false)
                 .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events")
                     .contentType(MediaType.APPLICATION_JSON)
@@ -55,6 +63,10 @@ public class EventControllerTests {
                 .andExpect(jsonPath("id").exists())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
                 .andExpect(header().string("Content-Type", "application/hal+json"))
+
+                // 아래 주석은 해당 값이 존재하지 않아야 한다. 존재할 시 오류 ㅂ라생
+                //.andExpect(jsonPath("id").value(Matchers.not(100)))
+                //.andExpect(jsonPath("free").value(Matchers.not(true)))
         ;
     }
 
