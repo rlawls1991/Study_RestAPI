e8be85c63205645eee6dd552dcdc63062470e02c
rlawls1991 <rlawls1991@gmail.com>
2021-01-21 19:37:32 +0900

    test - WebMvcTest 대신 SpringBootTest, AutoConfigureMockMvc 사용

--- src/test/java/study/events/EventControllerTests.java
+++ src/test/java/study/events/EventControllerTests.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -23,7 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTests {
 
     @Autowired
@@ -32,12 +35,10 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    EventRepository eventRepository;
-
     @Test
     public void createEvent() throws Exception {
         Event event = Event.builder()
+                .id(100)
                 .name("Spring")
                 .description("Rest API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2021, 01, 20, 18, 47))
@@ -51,8 +52,6 @@ public class EventControllerTests {
                 .free(true)
                 .offline(false)
                 .build();
-        event.setId(10);
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events")
                     .contentType(MediaType.APPLICATION_JSON)
@@ -63,10 +62,8 @@ public class EventControllerTests {
                 .andExpect(jsonPath("id").exists())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
                 .andExpect(header().string("Content-Type", "application/hal+json"))
-
-                // 아래 주석은 해당 값이 존재하지 않아야 한다. 존재할 시 오류 ㅂ라생
-                //.andExpect(jsonPath("id").value(Matchers.not(100)))
-                //.andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
         ;
     }
 
