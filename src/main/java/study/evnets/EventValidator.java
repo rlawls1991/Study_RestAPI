bc170155f49d94fc334bbbbc17cf9e73aba0ef58
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-22 15:10:07 +0900

    feat - EventDto에 담겨있는 값 검사 객체

---
+++ src/main/java/study/evnets/EventValidator.java
@@ -0,0 +1,33 @@
+package study.evnets;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDto eventDto, Errors errors) {
+        comparePrice(eventDto, errors);
+        compareDate(eventDto, errors);
+    }
+
+    private void comparePrice(EventDto eventDto, Errors errors) {
+        if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
+            errors.rejectValue("basePrice", "wrongValue", "BasePrice is worng.");
+            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is worng.");
+        }
+    }
+
+    private void compareDate(EventDto eventDto, Errors errors) {
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+
+        if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getBeginEventDateTime())
+        ) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "EndEventDateTime is worng.");
+        }
+    }
+}
