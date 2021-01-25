b83665ec991a2807d1ffb3ee5f741a44b2f75920
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-25 14:15:40 +0900

    test - IndexControllerTest 개발

---
+++ src/test/java/study/index/IndexControllerTest.java
@@ -0,0 +1,34 @@
+package study.index;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import study.events.common.RestDocsConfiguration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
+public class IndexControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void index() throws Exception {
+        this.mockMvc.perform(get("/api/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("_links.events").exists());
+    }
+
+}
