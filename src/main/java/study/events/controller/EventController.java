138667aefce636b62f88a9ce40348d2c6df81b7f
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-27 14:59:19 +0900

    feat - 이벤트 조회 api 개발(한개만)

--- src/main/java/study/events/controller/EventController.java
+++ src/main/java/study/events/controller/EventController.java
@@ -10,15 +10,13 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import study.events.common.ErrorsResource;
 import study.events.domain.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
@@ -71,6 +69,19 @@ public class EventController {
         var pageResource = assembler.toModel(page, e -> new EventResource(e));
         pageResource.add(Link.of("/docs/index.html#resources-events-list").withRel("profile"));
         return ResponseEntity.ok(pageResource);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getEvent(@PathVariable Integer id){
+        Optional<Event> optionalEvent = this.eventRepository.findById(id);
+        if(optionalEvent.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Event event = optionalEvent.get();
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(Link.of("/docs/index.html#resources-events-list").withRel("profile"));
+        return ResponseEntity.ok(eventResource);
     }
 
     private ResponseEntity<?> badRequest(Errors errors) {
