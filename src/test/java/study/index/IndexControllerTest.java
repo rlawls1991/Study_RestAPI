58c4d03b98676f05990821df5e4f65c4673283bd
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-27 12:45:14 +0900

    feat - @RunWith(SpringRunner.class) 어노테이션 추가

--- src/test/java/study/index/IndexControllerTest.java
+++ src/test/java/study/index/IndexControllerTest.java
@@ -1,12 +1,13 @@
 package study.index;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import study.events.common.RestDocsConfiguration;
 
@@ -14,11 +15,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
 public class IndexControllerTest {
 
     @Autowired
