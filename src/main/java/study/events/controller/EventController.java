eadd65b9ca585b89ba3dd00ce585853af4f218fe
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-27 13:52:26 +0900

    feat - pageable, PagedResourcesAssembler를 사용하여 pasing구현

--- src/main/java/study/events/controller/EventController.java
+++ src/main/java/study/events/controller/EventController.java
@@ -1,16 +1,22 @@
 package study.events.controller;
 
-import study.events.common.ErrorsResource;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import study.events.common.ErrorsResource;
 import study.events.domain.*;
 
 import javax.validation.Valid;
@@ -59,6 +65,15 @@ public class EventController{
         log.info("eventResource =:> " , eventResource);
         return ResponseEntity.created(createdUri).body(eventResource);
     }
+
+    @GetMapping
+    public ResponseEntity queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler){
+        Page<Event> page = this.eventRepository.findAll(pageable);
+        PagedModel<EntityModel<Event>> pageResource = assembler.toModel(page, e -> new EventResource(e));
+        pageResource.add(Link.of("/docs/index.html#resources-events-list").withRel("profile"));
+        return ResponseEntity.ok(pageResource);
+    }
+
 
     private ResponseEntity<?> badRequest(Errors errors) {
         return ResponseEntity.badRequest().body(new ErrorsResource(errors));
