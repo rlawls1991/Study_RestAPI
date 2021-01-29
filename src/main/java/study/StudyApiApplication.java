cdfc8ee0eee23b1710e731da7302a51c21c07db5
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-29 13:14:40 +0900

    feat - Appconfig 에 Bean설정 부분 이동

--- src/main/java/study/StudyApiApplication.java
+++ src/main/java/study/StudyApiApplication.java
@@ -4,16 +4,15 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import study.config.AppConfig;
 
 @SpringBootApplication
-public class StudyApiApplication {
+public class StudyApiApplication extends AppConfig {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyApiApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
 }
