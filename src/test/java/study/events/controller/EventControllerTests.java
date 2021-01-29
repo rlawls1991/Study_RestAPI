cf332c19a49d650424e9b52e294bb50a82651b0d
rlawls1991 <rlawls1991@gmail.com>
2021-01-29 21:03:43 +0900

    test - AuthenticationPrincipal 사용 테스트

--- src/test/java/study/events/controller/EventControllerTests.java
+++ src/test/java/study/events/controller/EventControllerTests.java
@@ -248,6 +248,34 @@ public class EventControllerTests extends BaseControllerTest {
     }
 
     @Test
+    @DisplayName("30개의 이벤트를 10개식 두번째 페이지 조회하기")
+    public void generateEventWithAuthentication() throws Exception {
+        // Given
+        /*
+        IntStream.range(0, 30).forEach(i ->{
+            this.generateEvent(i);
+        });
+        */
+        IntStream.range(0, 30).forEach(this::generateEvent);
+
+        // When
+        this.mockMvc.perform(get("/api/events")
+                .header(HttpHeaders.AUTHORIZATION, getBearerToken())
+                .param("page", "1")
+                .param("size", "10")
+                .param("sort", "name,DESC"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("page").exists())
+                .andExpect(jsonPath("_embedded.eventList[0]._links.self").exists())
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.profile").exists())
+                .andExpect(jsonPath("_links.create-event").exists())
+                .andDo(document("query-events"))
+        ;
+    }
+
+    @Test
     @DisplayName("기존의 이벤트를 하나 조회하기")
     public void getEvent() throws Exception {
         // Given
