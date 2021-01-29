c2851626a56ccf4f91678b6bbfa5134ed6102dcd
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-29 14:34:05 +0900

    feat- AutoServerConfig 설정

---
+++ src/main/java/study/config/AutoServerConfig.java
@@ -0,0 +1,71 @@
+package study.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import study.accounts.service.AccountService;
+
+@Configuration
+@EnableAuthorizationServer
+public class AutoServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    TokenStore tokenStore;
+
+    /**
+     * 패스워드 설정
+     * clientSecret를 사용할 때 사용
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.passwordEncoder(passwordEncoder);
+    }
+
+    /**
+     * client 설정
+     * id 설정
+     * password 설정
+     * grant_type 설정
+     * scope 설정
+     * accessTokenValiditySeconds(액세스 토큰 유지 시간)
+     * refreshTokenValiditySeconds(리프레시 토큰 유지 시간)
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("myApp")
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("read", "write") // 정의 한대로
+                .secret(this.passwordEncoder.encode("pass"))
+                .accessTokenValiditySeconds(10 * 60)
+                .refreshTokenValiditySeconds(6 * 10 * 60);
+    }
+
+    /**
+     * thentiacationManager 설정
+     * UserDetailsService 설정
+     * TokenStored 설정
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(accountService)
+                .tokenStore(tokenStore);
+    }
+}
