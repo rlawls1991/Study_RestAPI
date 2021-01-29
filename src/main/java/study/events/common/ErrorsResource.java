14c50c34cfff14a9181aa946d7d801cd08dd77c0
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-29 13:13:19 +0900

    feat - modelOf 삭제

--- src/main/java/study/events/common/ErrorsResource.java
+++ src/main/java/study/events/common/ErrorsResource.java
@@ -1,23 +1,17 @@
 package study.events.common;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
-import study.index.IndexController;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.validation.Errors;
+import study.index.IndexController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 public class ErrorsResource extends EntityModel<Errors> {
 
     public ErrorsResource(Errors content, Link... links) {
         super(content, links);
         add(linkTo(methodOn(IndexController.class).index()).withRel("index"));
-    }
-
-    public static EntityModel<Errors> modelOf(Errors errors) {
-        EntityModel<Errors> errorsModel = EntityModel.of(errors);
-        errorsModel.add(linkTo(methodOn(IndexController.class).index()).withRel("index"));
-        return errorsModel;
     }
 }
