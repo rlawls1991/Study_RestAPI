15af8b0c942fe62473fdbda2c0b722b7be64b37b
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2021-01-29 13:12:55 +0900

    feat - modelMapper, passwordEncoder,applicationRunner 설정

--- src/main/java/study/config/AppConfig.java
+++ src/main/java/study/config/AppConfig.java
@@ -1,17 +1,50 @@
 package study.config;
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import study.accounts.domain.Account;
+import study.accounts.domain.AccountRole;
+import study.accounts.service.AccountService;
+
+import java.util.Set;
 
 @Configurable
 public class AppConfig {
 
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account user = Account.builder()
+                        .email("jinSeok@test.com")
+                        .password("jinSeok")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(user);
+            }
+        };
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
     }
 }
