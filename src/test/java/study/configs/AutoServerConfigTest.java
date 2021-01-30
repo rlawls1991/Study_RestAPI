a8a783dc7f89c0001ffb1fc72d28e163bb716067
rlawls1991 <rlawls1991@gmail.com>
2021-01-30 19:04:44 +0900

    test - token 오류 수정

--- src/test/java/study/configs/AutoServerConfigTest.java
+++ src/test/java/study/configs/AutoServerConfigTest.java
@@ -1,16 +1,12 @@
 package study.configs;
 
 
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import study.accounts.domain.Account;
-import study.accounts.domain.AccountRole;
-import study.accounts.service.AccountService;
 import study.common.BaseControllerTest;
 import study.config.AppProperties;
-
-import java.util.Set;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,26 +17,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AutoServerConfigTest extends BaseControllerTest {
 
     @Autowired
-    AccountService accountService;
-
-
-    @Autowired
     AppProperties appProperties;
-
 
     @Test
     @DisplayName("인증 토큰을 발급 받는 테스트")
     public void getAuthToken() throws Exception {
-        String userName = appProperties.getUserUserName();
-        String password = appProperties.getUserPassword();
-
-        Account jinSeok = Account.builder()
-                .email(userName)
-                .password(password)
-                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
-                .build();
-        this.accountService.saveAccount(jinSeok);
-
         this.mockMvc.perform(post("/oauth/token")
                 .with(httpBasic(appProperties.getClientId(), appProperties.getClientSecret()))
                 .param("username", appProperties.getUserUserName())
